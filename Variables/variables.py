acc85c482a730815bc8030b17540f39652defbec
purplefeetguy <purplefeetguy@hotmail.com>
2018-02-04 10:30:11 -0600

    finished first variables video

--- Variables/variables.py
+++ Variables/variables.py
@@ -1,13 +1,14 @@
-greeting = "Bruce"
-_myName = "Tim"
-Tim45 = "Good"
-Tim_Was_57 = "Hello"
-Greeting = "There"
-print(Tim_Was_57 + ' ' + greeting)
-
-age = 24
-print(age)
-
-print(greeting + age)
+a = 12
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
 
 
+for i in range(1, a//b):
+    print(i)
+
+print((((a + b) / 3) -4) * 12)
