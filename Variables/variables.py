0ecf5e3f99861b631090e88bb77126f293c976b3
purplefeetguy <purplefeetguy@hotmail.com>
2018-02-11 21:21:47 -0600

    updating files

--- Variables/variables.py
+++ Variables/variables.py
@@ -1,19 +1,21 @@
-a = 12
-b = 3
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
-print(a // b)
-print(a % b)
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[0])
+print(parrot[3])
+#print(parrot[14])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:6:2])
+print(parrot[0:6:3])
 
-
-for i in range(1, a//b):
-    print(i)
-
-print((((a + b) / 3) -4) * 12)
-
-c = a + b
-d = c / 3
-e = d - 4
-print(e * 12)
+number = "9,223,372,036,854,775,807"
+print(number[1::4])
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(numbers[0::3])
+string1 = "he's "
+string2 = "probably "
+print(string1 + string2)
+print("he's probably " "pining")
